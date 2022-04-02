2ce0ec7ba35dbf93f4c0ff8c2b50b76db267a7bc
Sahil Patel <sp0715007@gmail.com>
2022-04-02 04:19:37 -0400

    Added React Context to give the value to Carousel

--- src/Page/Home.js
+++ src/Page/Home.js
@@ -2,6 +2,12 @@ import React, { useState, useEffect } from "react";
 import Carousel from '../components/Carousel/Carousel.component'
 import MenuButton from "../components/Button/MenuButton.component";
 
+
+export const DataContext = React.createContext({
+    data: null,
+    isFetching: false
+})
+
 const Home = () => {
   const [showShark, setShowShark] = useState(false);
   const [showCat, setShowCat] = useState(false);
@@ -61,6 +67,11 @@ const Home = () => {
       <div className="buttonContainer">
         <MenuButton name="Cat" active={showCat} handleClick={catClick} />
         <MenuButton name="Shark" active={showShark} handleClick={sharkClick} />
+      </div>
+      <div>
+          <DataContext.Provider value={setShowImages}>
+              <Carousel />
+          </DataContext.Provider>
       </div>
     </div>
   );
